f2eb12c9751d2e288700ae65b535f421ba301ade
Cameron MacLeod <cmacleod170@googlemail.com>
2020-02-15 17:46:35 +0000

    Install/execute with correct path

--- profile.py
+++ profile.py
@@ -117,8 +117,8 @@ kube_m.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU16-64-ST
 #kube_m.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD'
 kube_m.Site('Site 1')
 iface0 = kube_m.addInterface('interface-0')
-kube_m.addService(pg.Install('https://github.com/notexactlyawe/honours-project/archive/master.tar.gz', '/local/honours-project/'))
-kube_m.addService(pg.Execute(shell="bash", command="/local/honours-project/scripts/master.sh"))
+kube_m.addService(pg.Install('https://github.com/notexactlyawe/honours-project/archive/master.tar.gz', '/local/'))
+kube_m.addService(pg.Execute(shell="bash", command="/local/honours-project-master/scripts/master.sh"))
 
 slave_ifaces = []
 for i in range(1,params.computeNodeCount+1):
@@ -133,8 +133,8 @@ for i in range(1,params.computeNodeCount+1):
     #kube_s.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD'
     kube_s.Site('Site 1')
     slave_ifaces.append(kube_s.addInterface('interface-'+str(i)))
-    kube_s.addService(pg.Install('https://github.com/notexactlyawe/honours-project/archive/master.tar.gz', '/local/honours-project/'))
-    kube_s.addService(pg.Execute(shell="bash", command="/local/honours-project/scripts/slave.sh"))
+    kube_s.addService(pg.Install('https://github.com/notexactlyawe/honours-project/archive/master.tar.gz', '/local/'))
+    kube_s.addService(pg.Execute(shell="bash", command="/local/honours-project-master/scripts/slave.sh"))
 
 # Link link-m
 link_m = request.Link('link-0')
